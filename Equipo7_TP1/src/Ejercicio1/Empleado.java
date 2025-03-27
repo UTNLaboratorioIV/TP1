8e44c925c1f9deabc09abeb9a8ef633c27b3532c
itati3911 <itati3911@gmail.com>
2025-03-27 19:34:17 -0300

    Añado a class Empleado Atributos, Constructores, proximoID Añado main a class Principal

--- Equipo7_TP1/src/Ejercicio1/Empleado.java
+++ Equipo7_TP1/src/Ejercicio1/Empleado.java
@@ -1,5 +1,24 @@
 package Ejercicio1;
 
 public class Empleado {
+	
+	//atributos
+	private final int id;
+	private int proximoID = 1000;
+	private String nombre;
+	private int edad;
+	
+	//constructores
+	public Empleado() {
+		id = proximoID++;
+		nombre = "sin nombre";
+		edad = 99;
+	}
+	
+	public Empleado(String nombre, int edad) {
+		this.id = proximoID++;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
 
 }
