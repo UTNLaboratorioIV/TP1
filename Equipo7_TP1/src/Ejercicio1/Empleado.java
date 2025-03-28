634be292ccc69a37171b22de0ab3754ee1a8664d
MaximilianoAns <127036656+MaximilianoAns@users.noreply.github.com>
2025-03-28 20:30:13 -0300

    Añado funcion statica para generar el proximo id en el Main

--- Equipo7_TP1/src/Ejercicio1/Empleado.java
+++ Equipo7_TP1/src/Ejercicio1/Empleado.java
@@ -4,11 +4,14 @@ public class Empleado {
 	
 	//atributos
 	private final int id;
-	private int proximoID = 1000;
+	private static int proximoID = 1000;
 	private String nombre;
 	private int edad;
 	
 	//constructores
+	public static  int DevuelveProximoID() {
+		return proximoID+1;
+	}
 	public Empleado() {
 		id = proximoID++;
 		nombre = "sin nombre";
@@ -20,5 +23,26 @@ public class Empleado {
 		this.nombre = nombre;
 		this.edad = edad;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
 
 }
---
+++ Equipo7_TP1/src/Ejercicio1/Main.java
@@ -0,0 +1,9 @@
+package Ejercicio1;
+
+public class Main {
+	public static void main(String []args) {	
+		
+		System.out.println("El proximo ID a generarse es: " + Empleado.DevuelveProximoID());
+		
+	}
+}
